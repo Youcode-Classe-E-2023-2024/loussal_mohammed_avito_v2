37841054d6bf4ebac022e2f42ec2b3d101f6f3bd
PythonScript1 <hplaptopsimo@gmail.com>
2023-11-28 15:47:51 +0100

    adding register autoload to bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,14 @@
 <?php
-// Loud Libraries:
-require_once 'libraries/controller.php';
-require_once 'libraries/core.php';
-require_once 'libraries/database.php';
+  // Load Config
+  require_once 'config/config.php';
 
+  // Load Helpers
+  require_once 'helpers/url_helper.php';
+  require_once 'helpers/session_helper.php';
+
+  // Autoload Core Libraries
+  spl_autoload_register(function($className){
+    require_once 'libraries/' . $className . '.php';
+  });
+  
+?>
