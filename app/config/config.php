ed7f03b3349d1cf6cb73c2525cdd36bf2e55a4e3
PythonScript1 <hplaptopsimo@gmail.com>
2023-11-28 15:30:24 +0100

    defining contants for host and user and pass and db_name and approot and urlroot and sitename

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+// DB params:
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "112358");
+define("DB_NAME", "loussalmohammedavitov2");
+
+// APP Root:
+define("APPROOT", dirname(dirname(__FILE__)));
+
+// URL Root:
+define("URLROOT", "http://localhost/LOUSSAL_MOHAMMED_AVITO_V2");
+
+// SITE Name:
+define("SITENAME", "LMavito");
+
+
+?>
